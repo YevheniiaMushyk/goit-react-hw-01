ef9729d8327a6131248d6c611d54f3ca4c3f9754
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-02-24 16:40:09 +0200

    add composes

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,11 +1,10 @@
 import css from "../components/FriendListItem.module.css";
-import clsx from "clsx";
 const FriendListItem = ({ avatar, name, isOnline }) => {
 	return (
 		<div className={css.card}>
 			<img src={avatar} alt="Avatar" width="80" />
 			<p className={css.name}>{name}</p>
-			<p className={clsx(css.status, isOnline && css.statusonline)}>{isOnline ? "Online" : "Offline"}</p>
+			<p className={isOnline ? css.statusonline : css.statusoffline}>{isOnline ? "Online" : "Offline"}</p>
 		</div>
 	);
 };
